668342baead5ff70691ea3302b145c554980755e
dtellez-wm <david.tellez@webmaps.com.mx>
2022-02-18 16:30:49 -0600

    Se ajustan las lineas dinamicamente tomando como coordenadas los puntos

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,26 +3,25 @@ document.body.appendChild(app.view);
 
 const graphics = new PIXI.Graphics();
 
-
 const puntos = [{
     id : 1,
-    x : 10,
-    y : 400,
-},
-{
-    id : 2,
-    x : 40,
-    y : 250,
-},
-{
-    id : 3,
-    x : 200,
+    x : 150,
     y : 150,
 },
 {
-    id : 4,
-    x : 160,
+    id : 2,
+    x : 300,
     y : 200,
+},
+{
+    id : 3,
+    x : 400,
+    y : 400,
+},
+{
+    id : 4,
+    x : 600,
+    y : 400,
 }
 ]
 
@@ -32,6 +31,7 @@ for (i = 0; i < puntos.length; i++) {
     let value_x = puntos[i].x;
     let value_y = puntos[i].y;
     dibujar_circulo(value_x, value_y);
+    dibujar_linea(value_x, value_y);
 }
 
 function dibujar_circulo(value_x, value_y) {
@@ -40,17 +40,15 @@ function dibujar_circulo(value_x, value_y) {
     graphics.beginFill(0xDE3239, 1);
     graphics.drawCircle(value_x, value_y, 5);
     graphics.endFill();
-    }
+}
 
-    const realPath = new PIXI.Graphics();
+function dibujar_linea(value_x, value_y) {
+    graphics.lineStyle(2, 0xDE9239, 1);
+    graphics.moveTo(puntos[0].x, puntos[0].y);
+    graphics.lineTo(value_x, value_y);
+}
 
-    realPath.lineStyle(2, 0xDE9239, 1);
-    realPath.moveTo(10, 400);
-    realPath.lineTo(40, 250);
-    realPath.lineTo(200, 150);
-    realPath.lineTo(160, 200);
-    
+const realPath = new PIXI.Graphics();
     
 app.stage.addChild(realPath);
-
 app.stage.addChild(graphics);
